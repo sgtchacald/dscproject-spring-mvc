b17169536d488a1b31c17af7f298b4a6491f3eac
dscordeiro <sgt.chacal.d@gmail.com>
2026-03-22 22:10:54 -0300

    feat: commit inicial do projeto

---
+++ src/main/java/br/com/diegocordeiro/dscproject/DscprojectApplication.java
@@ -0,0 +1,13 @@
+package br.com.diegocordeiro.dscproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/diegocordeiro/dscproject/DscprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diegocordeiro.dscproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
