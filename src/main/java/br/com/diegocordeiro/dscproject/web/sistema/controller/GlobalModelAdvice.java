8ba7c51db4446f48735796acb10979d1d37ac394
dscordeiro <sgt.chacal.d@gmail.com>
2026-09-07 01:25:03 -0300

    refactor: lê a versão da aplicação do filtro do Maven
    Troca a dependência de BuildProperties (que exige o goal build-info do
    spring-boot-maven-plugin) por ${app.version}, preenchido pelo Maven a partir
    de ${project.version} via filtragem de recursos do spring-boot-starter-parent.

--- src/main/java/br/com/diegocordeiro/dscproject/web/sistema/controller/GlobalModelAdvice.java
+++ src/main/java/br/com/diegocordeiro/dscproject/web/sistema/controller/GlobalModelAdvice.java
@@ -1,18 +1,20 @@
 package br.com.diegocordeiro.dscproject.web.sistema.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.info.BuildProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @ControllerAdvice
 public class GlobalModelAdvice {
 
-    @Autowired
-    private BuildProperties buildProperties;
+    private final String appVersao;
+
+    public GlobalModelAdvice(@Value("${app.version:dev}") String appVersao) {
+        this.appVersao = appVersao;
+    }
 
     @ModelAttribute("appVersao")
     public String appVersao() {
-        return "v" + buildProperties.getVersion();
+        return "v" + appVersao;
     }
 }
